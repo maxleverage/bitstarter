7781af0c9c1c332b743ddb4422c61db93b0ba647
Ubuntu <ubuntu@ip-172-31-28-94.sa-east-1.compute.internal>
2013-07-23 21:41:53 +0000

    Revised web.js file referencing index.html file

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
 var fs = require('fs');
 var file_default = "index.html";
 file = file || file_default;
 var buffer = new Buffer(fs.readFileSync(file));
 
-app.use(express, logger());
-
-// Output
 app.get('/', function(request, response) {
 	response.send(buffer.toString());
 });
