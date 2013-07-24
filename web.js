27bf1029cdbf2562ff8fe2e90c72dc7bbe240f6d
Henry Wang <henrywang7@gmail.com>
2013-07-24 04:27:25 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
-var express = require('express');
+var express = express('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var content = fs.readFileSync('index.html', 'utf8');
+	var fs = require('fs');
+	var buf = new Buffer(fs.readFileSync('index.html'), "utf-8");
+	response.send(buf.toString());
 });
 
-var port = process,env,PORT || 5000;
-app.list(port, function() {
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
 	console.log("Listening on " + port);
 });
 
