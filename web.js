25fe36f561e0dad9d39f8323fea733aef5b4b751
Henry Wang <henrywang7@gmail.com>
2013-07-25 16:44:02 +0000

    Modified web.js to remove error

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
-var express = require("express");
-var app = express.createServer(express.logger());
+var express = require('express');
+var fs = require('fs');
+var app = express(express.logger());
 
 app.get('/', function(request, response) {
-	var fs = require('fs');
-		var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-		response.send(buf.toString());
+	var data = fs.readFileSync('index.html');
+	var buf = new Buffer(data, 'utf-8');
+	response.send(buf.toString());
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
+
 
