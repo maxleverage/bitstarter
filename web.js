1745e410981190e5e7d5b502f7a27d20e7c86717
Henry Wang <henrywang7@gmail.com>
2013-07-25 02:54:12 +0000

    Modified express

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 		response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
